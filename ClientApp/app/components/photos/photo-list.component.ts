303001bfcc6fbb102b942c566c77f401b3dd6bd2
Kieran Harkin <kieranharkin@googlemail.com>
2017-06-27 00:03:15 +0100

    init

---
+++ ClientApp/app/components/photos/photo-list.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    templateUrl: './photo-list.component.html'
+})
+
+export class PhotoListComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
